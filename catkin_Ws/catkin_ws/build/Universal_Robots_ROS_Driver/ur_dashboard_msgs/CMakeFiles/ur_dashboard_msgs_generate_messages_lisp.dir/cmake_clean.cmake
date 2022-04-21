b5148795af94bc6e3633ef086eddf1cfaf19ec36
asnackbar <56302911+asnackbar@users.noreply.github.com>
2022-04-21 14:58:12 +0200

    catkin added
    added catkin workspace to github

---
+++ catkin_Ws/catkin_ws/build/Universal_Robots_ROS_Driver/ur_dashboard_msgs/CMakeFiles/ur_dashboard_msgs_generate_messages_lisp.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/ProgramState.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/RobotMode.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SafetyMode.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeAction.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeActionFeedback.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeActionGoal.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeActionResult.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeFeedback.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeGoal.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/msg/SetModeResult.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/AddToLog.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/GetLoadedProgram.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/GetProgramState.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/GetRobotMode.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/GetSafetyMode.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/IsProgramRunning.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/IsProgramSaved.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/Load.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/Popup.lisp"
+  "/home/snook/catkin_ws/devel/share/common-lisp/ros/ur_dashboard_msgs/srv/RawRequest.lisp"
+  "CMakeFiles/ur_dashboard_msgs_generate_messages_lisp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ur_dashboard_msgs_generate_messages_lisp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ catkin_Ws/catkin_ws/build/ur5uia/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/snook/catkin_ws/src/ur5uia
+# Build directory: /home/snook/catkin_ws/build/ur5uia
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
