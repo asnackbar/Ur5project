1a4055c58e41bc937506b3e5407babe072ba6157
Snackbar <56302911+asnackbar@users.noreply.github.com>
2022-05-05 15:32:13 -0700

    catkin_ws changes
    removed wrong catkin

--- catkin_Ws/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/snook/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/snook/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/snook/catkin_ws/devel;/opt/ros/noetic' != '' else []
--- catkin_Ws/catkin_ws/build/ur5uia/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/snook/catkin_ws/src/ur5uia
-# Build directory: /home/snook/catkin_ws/build/ur5uia
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
